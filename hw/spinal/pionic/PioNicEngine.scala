5ae54667fe89dc64d837e21140ab78e1d62eff17
Pengcheng Xu <pengxu@ethz.ch>
2023-12-08 16:06:08 +0100

    annotate config better

--- hw/spinal/pionic/PioNicEngine.scala
+++ hw/spinal/pionic/PioNicEngine.scala
@@ -9,15 +9,22 @@ import spinal.lib._
 import spinal.lib.bus.amba4.axi._
 import spinal.lib.bus.amba4.axis._
 
-case class PioNicConfig(axiConfig: Axi4Config = Axi4Config(32, 64, 4),
-                        axisConfig: Axi4StreamConfig = Axi4StreamConfig(64),
-                        regWidth: Int = 64,
-                        pktBufAddrWidth: Int = 16, // 64KB
-                        pktBufSizePerCore: Int = 16 * 1024, // 16KB
-                        mtu: Int = 9600,
-                        maxRxPktsInFlight: Int = 128,
-                        maxRxBlockCycles: Int = 10000,
-                        numCores: Int = 4,
+case class PioNicConfig(
+                         axiConfig: Axi4Config = Axi4Config(
+                           addressWidth = 64,
+                           dataWidth = 512,
+                           idWidth = 4,
+                         ),
+                         axisConfig: Axi4StreamConfig = Axi4StreamConfig(
+                           dataWidth = 64, // BYTES
+                         ),
+                         regWidth: Int = 64,
+                         pktBufAddrWidth: Int = 16, // 64KB
+                         pktBufSizePerCore: Int = 16 * 1024, // 16KB
+                         mtu: Int = 9600,
+                         maxRxPktsInFlight: Int = 128,
+                         maxRxBlockCycles: Int = 10000,
+                         numCores: Int = 4,
                        )
 
 class PioNicEngine(implicit config: PioNicConfig) extends Component {
