0acf83da1a8acfc46d0aa48c7ba6a8ec7a48b41d
Pengcheng Xu <pengxu@ethz.ch>
2024-01-05 11:33:39 +0100

    allow wrapping around

--- hw/spinal/pionic/package.scala
+++ hw/spinal/pionic/package.scala
@@ -35,8 +35,11 @@ package object pionic {
     def apply[T <: Data](input: Stream[T], outputCount: Int, enableMask: Bits): Vec[Stream[T]] = {
       // FIXME: same as OHMasking.roundRobin?
       // FIXME: first packet always goes to core 0
+      assert(outputCount == enableMask.getWidth, "enable mask bit width does not match with output count")
       val select = Reg(UInt(log2Up(outputCount) bits)) init 0
-      val inc = CTZ(enableMask >> (select + 1)) + 1
+      val doubleMask = enableMask ## enableMask
+      val shiftedMask = doubleMask >> (select + 1)
+      val inc = CTZ(shiftedMask.resize(outputCount)) + 1
       when(input.fire) {
         select := select + inc.resized
       }
