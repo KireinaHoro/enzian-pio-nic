c3a19c16180be189b4104752e8814afb0a8671ca
Pengcheng Xu <pengxu@ethz.ch>
2024-02-28 11:55:06 +0100

    use setup phase for io creation

--- hw/spinal/pionic/NicEngine.scala
+++ hw/spinal/pionic/NicEngine.scala
@@ -15,8 +15,6 @@ object NicEngine {
     val n = new NicEngine
     n.host.asHostOf(plugins :+ new FiberPlugin {
       during build {
-        plugins.map(_.asInstanceOf[FiberPlugin].postBuildLock).foreach(_.await())
-
         // rename ports so Vivado could infer interfaces automatically
         renameAxi4IO()
         renameAxi4StreamIO(n, alwaysAddT = true)
