5afcea601c66d64b7fb68153fc1197abf257fee9
Pengcheng Xu <pengxu@ethz.ch>
2024-03-26 12:46:13 +0100

    allow setting the random seed for reproducible test failures

--- hw/spinal/pionic/sim/SimApp.scala
+++ hw/spinal/pionic/sim/SimApp.scala
@@ -1,23 +1,56 @@
 package pionic.sim
 
+import mainargs._
 import pionic.NicEngine
 import spinal.core.sim._
 
-abstract class SimApp extends App {
+import scala.collection.mutable
+import scala.util.Random
+
+trait SimApp extends DelayedInit {
   def dut: SimCompiled[NicEngine]
 
   def sleepCycles(n: Int)(implicit dut: NicEngine) = dut.clockDomain.waitActiveEdge(n)
 
-  if (args.length > 1) {
-    println(s"usage: ${getClass.getName} [test name pattern]")
-    sys.exit(1)
-  }
+  @main
+  def run(
+           @arg(doc = "regex of test names to include")
+           testPattern: String = ".*",
+           @arg(doc = "simulation random seed")
+           seed: Option[Int]
+         ): Unit = {
+    initCodes.foreach(_.apply())
 
-  def test(name: String)(body: NicEngine => Unit) = {
-    if (args.isEmpty || args.head.r.findFirstIn(name).nonEmpty) {
-      dut.doSim(name)(body)
-    } else {
-      println(s"[info] skipping test $name")
+    val globalSeed = seed.getOrElse(Random.nextInt)
+    Random.setSeed(globalSeed)
+
+    val p = testPattern.r
+
+    try {
+      tests.foreach { case (name, body) =>
+        p.findFirstIn(name) match {
+          case Some(_) =>
+            dut.doSim(name, seed = globalSeed)(body)
+          case None =>
+            println(s"[info] skipping test $name")
+        }
+      }
+    } catch {
+      case e: Throwable =>
+        println(s"[info] failed seed: $globalSeed")
+        throw e
     }
   }
+
+  private[this] val initCodes = mutable.ListBuffer[() => Unit]()
+
+  override def delayedInit(x: => Unit): Unit = initCodes += (() => x)
+
+  def test(name: String)(body: NicEngine => Unit): Unit = {
+    tests.append((name, body))
+  }
+
+  val tests = mutable.ListBuffer[(String, NicEngine => Unit)]()
+
+  final def main(args: Array[String]): Unit = ParserForMethods(this).runOrExit(args)
 }
