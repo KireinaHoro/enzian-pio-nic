187ff6a3097327bcf94eda6bdd691582322e4770
Pengcheng Xu <pengxu@ethz.ch>
2024-04-02 19:40:56 +0200

    move impl xdc constraints

--- vivado/eci/create_project.tcl
+++ vivado/eci/create_project.tcl
@@ -126,9 +126,13 @@ add_files -fileset [get_filesets sources_1] -norecurse \
 
 # Add constraints from us
 add_files -fileset [get_filesets constrs_1] -norecurse \
-    "$spinal_gen_dir/NicEngine.xdc" \
     "$hw_deps_dir/verilog-axis/syn/vivado/axis_async_fifo.tcl" \
     "$hw_deps_dir/verilog-axis/syn/vivado/sync_reset.tcl"
+
+# implementation-only constraints: copied to out dir
+set impl_constrs_dir "$project/xdc/impl"
+file mkdir $impl_constrs_dir
+file copy -force "$spinal_gen_dir/NicEngine.xdc" "$impl_constrs_dir"
 
 set_property "top" "${top_module}" [get_filesets sources_1]
 
