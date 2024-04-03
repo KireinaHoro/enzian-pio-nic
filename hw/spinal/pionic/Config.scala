022fbe7494377d63dfee00763a6e950cfb956b40
Pengcheng Xu <pengxu@ethz.ch>
2024-04-03 14:18:17 +0200

    use xpm blackboxing

--- hw/spinal/pionic/Config.scala
+++ hw/spinal/pionic/Config.scala
@@ -1,21 +1,26 @@
 package pionic
 
 import spinal.core._
+import spinal.core.internals.Phase
 import spinal.core.sim._
 
+import jsteward.blocks.mem.xpm.PhaseMemBlackBoxingXpm
+
+import scala.collection.mutable.ArrayBuffer
 import scala.language.postfixOps
 
 object Config {
   val outputDirectory = "hw/gen"
 
-  def spinal(outDir: String = outputDirectory, blackboxPolicy: MemBlackboxingPolicy = blackboxAllWhatsYouCan) = SpinalConfig(
+  def spinal(outDir: String = outputDirectory, blackboxPolicy: MemBlackboxingPolicy = blackboxAll) = SpinalConfig(
     targetDirectory = outDir,
+    memBlackBoxers = ArrayBuffer[Phase](new PhaseMemBlackBoxingXpm(blackboxPolicy)),
     defaultConfigForClockDomains = ClockDomainConfig(
       resetActiveLevel = HIGH,
       resetKind = SYNC,
     ),
     onlyStdLogicVectorAtTopLevelIo = true
-  ).addStandardMemBlackboxing(blackboxPolicy)
+  )
 
   def sim = SimConfig.withConfig(spinal(blackboxPolicy = blackboxOnlyIfRequested))
     .withFstWave
