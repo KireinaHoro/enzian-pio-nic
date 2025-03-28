a567662352892673a8ce905163139e432c7615a0
KireinaHoro <i@jsteward.moe>
2025-03-28 21:29:42 +0100

    preempt control WIP

--- hw/spinal/pionic/host/eci/EciPreemptionControlPlugin.scala
+++ hw/spinal/pionic/host/eci/EciPreemptionControlPlugin.scala
@@ -43,13 +43,19 @@ class EciPreemptionControlPlugin(val coreID: Int) extends PreemptionService {
     val lcia = Stream(EciCmdDefs.EciAddress)
     val ul = Stream(EciCmdDefs.EciAddress)
 
-    // TODO
-    lci.setIdle()
-    ul.setIdle()
-    lcia.setBlocked()
+    lci.valid := False
+    lci.payload := controlClAddr
 
-    val preemptCtrlCl = Reg(PreemptionControlCl()) init 0
-    assert(requiredAddrSpace >= preemptCtrlCl.getWidth, "not all information fits in the preemption control CL")
+    ul.payload := controlClAddr
+    ul.valid := False
+
+    lcia.setBlocked()
+    when (lcia.valid) {
+      assert(lcia.payload === controlClAddr, "LCIA does not match control address!")
+    }
+
+    val preemptCtrlCl = Reg(PreemptionControlCl())
+    assert(requiredAddrSpace >= preemptCtrlCl.getBitsWidth, "not all information fits in the preemption control CL")
 
     // Preemption request from scheduler.  When issued (valid === True), the scheduler
     // must have:
@@ -59,7 +65,7 @@ class EciPreemptionControlPlugin(val coreID: Int) extends PreemptionService {
     //
     // When [[preemptReq]] is acknowledged (valid && ready === True), the kernel would have
     // signalled that they finished all steps and will immediately return to user space.
-    // The scheduler can allow
+    // The scheduler can then allow new requests into the granted buffer.
     val preemptReq = Stream(PID())
     preemptReq.setBlocked()
 
@@ -70,8 +76,8 @@ class EciPreemptionControlPlugin(val coreID: Int) extends PreemptionService {
     // Parity bits to serve to the kernel in the preemption control cacheline.  These
     // are also accessible directly as register reads (exposed originally for debugging)
     // in [[EciDecoupledRxTxProtocol]] -- this optimization will save the extra I/O roundtrip
-    val rxParity = proto.logic.rxCurrClIdx
-    val txParity = proto.logic.txCurrClIdx
+    preemptCtrlCl.rxParity := proto.logic.rxCurrClIdx
+    preemptCtrlCl.txParity := proto.logic.txCurrClIdx
 
     // Preemption request to forward to the datapath.  Issued AFTER clearing READY bit
     // to ACK the pending packet (if any) and drop ctrl (& data, if any) CLs from L2 cache
@@ -80,41 +86,116 @@ class EciPreemptionControlPlugin(val coreID: Int) extends PreemptionService {
 
     val fsm = new StateMachine {
       val idle: State = new State with EntryPoint {
-        when (preemptReq.valid) {
-
-          goto(unsetReadyReq)
+        whenIsActive {
+          when (preemptReq.valid) {
+            lci.valid := True
+            when (lci.ready) {
+              goto(unsetReady)
+            }
+          }
         }
       }
-      val unsetReadyReq: State = new State {
-
-      }
       val unsetReady: State = new State {
-
+        whenIsActive {
+          lcia.freeRun()
+          when (lcia.valid) {
+            preemptCtrlCl.ready := False
+            when (preemptCtrlCl.busy) {
+              // still busy, give the CPU a chance to de-assert busy
+              goto(unsetReadyUnlock)
+            } otherwise {
+              // already not busy, don't unlock now
+              goto(preemptDataPath)
+            }
+          }
+        }
+      }
+      val unsetReadyUnlock: State = new State {
+        whenIsActive {
+          ul.valid := True
+          when (ul.ready) {
+            goto(preemptDataPath)
+          }
+        }
       }
       val preemptDataPath: State = new State {
-
+        whenIsActive {
+          rxProtoPreemptReq.valid := True
+          when (rxProtoPreemptReq.ready) {
+            when (preemptCtrlCl.busy) {
+              // busy and we unlocked -- poll again
+              goto(readBusyReq)
+            } otherwise {
+              // busy already low but locked, we can issue IPI
+              goto(issueIpi)
+            }
+          }
+        }
       }
       val readBusyReq: State = new State {
-
+        whenIsActive {
+          lci.valid := True
+          when (lci.ready) {
+            goto(readBusy)
+          }
+        }
       }
       val readBusy: State = new State {
-
+        whenIsActive {
+          lcia.freeRun()
+          when (lcia.valid) {
+            when (preemptCtrlCl.busy) {
+              // still busy, unlock and poll again
+              goto(readBusyUnlock)      
+            } otherwise {
+              // not busy anymore, but locked -- issue IPI
+              goto(issueIpi)
+            }
+          }
+        }
+      }
+      val readBusyUnlock: State = new State {
+        whenIsActive {
+          ul.valid := True
+          when (ul.ready) {
+            goto(readBusyReq)
+          }
+        }
       }
       val issueIpi: State = new State {
-
+        whenIsActive {
+          // TODO: how to issue IPI?
+          goto(setReady)
+        }
       }
       val setReady: State = new State {
-
+        whenIsActive {
+          preemptCtrlCl.ready := True
+          // we are guaranteed to be in kernel now, release PID
+          preemptCtrlCl.pid := preemptReq.payload
+          ul.valid := True
+          when (ul.ready) {
+            goto(waitKernelPreemptFinished)
+          }
+        }
       }
       val waitKernelPreemptFinished: State = new State {
-
+        whenIsActive {
+          // TODO: what to wait for here?  e.g. an I/O read/write that blocks?
+          when (True) {
+            preemptReq.ready := True
+            // no need to clear preemptCtrlCl.pid -- kernel will clear and then pin to L2
+            goto(idle)
+          }
+        }
       }
     }
   }
 
   override def preemptReq: Stream[PID] = logic.preemptReq
   def driveDcsBus(bus: Axi4, lci: Stream[Bits], lcia: Stream[Bits], ul: Stream[Bits]) = {
-    bus.read(controlClAddr, logic.preemptCtrlCl)
+    val busCtrl = Axi4SlaveFactory(bus)
+    busCtrl.readAndWrite(logic.preemptCtrlCl, controlClAddr)
 
     lci  << logic.lci
     ul   << logic.ul
