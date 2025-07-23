b0c2f864a391c20fa49d0f8a10c16b47ae0c6d6d
Zikai Liu <zikai.liu@outlook.com>
2025-07-23 12:23:15 +0200

    Update kmod

--- sw/kmod/main.c
+++ sw/kmod/main.c
@@ -50,17 +50,53 @@ static struct file_operations fops = {
     // .release        = etx_release,
 };
 
+
+
+
+
+
 static dev_t dev = 0;
 static struct cdev cdev;
 static struct class *dev_class;
 
+
+static int create_device(void) {
+  if (alloc_chrdev_region(&dev, 0, 1, "lauberhorn") < 0) {
+    pr_err("alloc_chrdev_region failed\n");
+    return -1;
+  }
+  pr_info("chrdev major = %d, minor = %d \n", MAJOR(dev), MINOR(dev));
+  cdev_init(&cdev, &fops);
+  if (cdev_add(&cdev, dev, 1) < 0) {
+    pr_err("cdev_add failed\n");
+    return -1;
+  }
+  if (IS_ERR(dev_class = class_create(THIS_MODULE, "lauberhorn_class"))) {
+    pr_err("class_create failed\n");
+    return -1;
+  }
+  if (IS_ERR(device_create(dev_class, NULL, dev, NULL, "lauberhorn"))) {
+    pr_err("device_create failed\n");
+    return -1;
+  }
+  pr_info("Device created at /dev/lauberhorn\n");
+  return 0;
+}
+
+static void remove_device(void) {
+  device_destroy(dev_class, dev);
+  class_destroy(dev_class);
+  cdev_del(&cdev);
+  unregister_chrdev_region(dev, 1);
+  pr_info("Device removed\n");
+}
+
+
 // Module initialization
 static int __init mod_init(void) {
-  int ret = 0;
+  pr_info("Lauberhorn kmod loading...\n");
 
-  pr_info("Loading...\n");
-
-  /* ktask = kthread_create(thread_function, NULL, "pionic_kthread");
+  /* ktask = kthread_create(thread_function, NULL, "lauberhorn_kthread");
   // TODO: kthread_bind to CPU
   if (ktask != NULL) {
     wake_up_process(ktask);
@@ -72,68 +108,25 @@ static int __init mod_init(void) {
   } */
 
   // Register the device
-
-  // Use char device for now...
-  if (alloc_chrdev_region(&dev, 0, 1, "pionic_device") < 0) {
-    pr_err("alloc_chrdev_region failed\n");
-    ret = -1;
-    goto err_alloc_chrdev_region;
-  }
-  pr_info("chrdev major = %d, minor = %d \n", MAJOR(dev), MINOR(dev));
-
-  cdev_init(&cdev, &fops);
-
-  if (cdev_add(&cdev, dev, 1) < 0) {
-    pr_err("cdev_add failed\n");
-    ret = -1;
-    goto err_cdev_add;
+  if (create_device() != 0) {
+    remove_device();
+    return -1;
   }
 
-  if (IS_ERR(dev_class = class_create(THIS_MODULE, "pionic_class"))) {
-    pr_err("class_create failed\n");
-    ret = -1;
-    goto err_class_create;
-  }
-
-  if (IS_ERR(device_create(dev_class, NULL, dev, NULL, "pionic"))) {
-    pr_err("device_create failed\n");
-    ret = -1;
-    goto err_device_create;
-  }
-  pr_info("Device created at /dev/pionic\n");
-
-  pr_info("Loaded\n");
-  goto done;
-
-  device_destroy(dev_class, dev);
-err_device_create:
-  class_destroy(dev_class);
-err_class_create:
-  cdev_del(&cdev);
-err_cdev_add:
-  unregister_chrdev_region(dev, 1);
-err_alloc_chrdev_region:
-/*   // delete the kthread...
-err_kthread_create: */
-done:
-  return ret;
+  pr_info("Lauberhorn kmod loaded\n");
+  return 0;
 }
 
 // Module exit
 static void __exit mod_exit(void) {
-  pr_info("Exiting...\n");
+  pr_info("Lauberhorn kmod exiting...\n");
 
-  // Remove the device
-  device_destroy(dev_class, dev);
-  class_destroy(dev_class);
-  cdev_del(&cdev);
-  unregister_chrdev_region(dev, 1);
-  pr_info("Device removed\n");
+  remove_device();
 
   /* ret = kthread_stop(ktask);
   pr_info("kthread returns %d", ret); */
 
-  pr_info("Unloaded\n");
+  pr_info("Lauberhorn kmod unloaded\n");
 }
 
 MODULE_AUTHOR("Zikai Liu");
