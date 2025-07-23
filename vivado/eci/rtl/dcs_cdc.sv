9777f65cb5a26a985cf6fd199b1cc43957671b93
Pengcheng Xu <pengxu@ethz.ch>
2025-07-23 15:23:12 +0200

    increase pipelining on CDC fifo

--- vivado/eci/rtl/dcs_cdc.sv
+++ vivado/eci/rtl/dcs_cdc.sv
@@ -12,7 +12,10 @@ axis_async_fifo #( \
   .DATA_WIDTH((data_width) + 4 + ECI_PACKET_SIZE_WIDTH), \
   .KEEP_ENABLE(0), \
   .LAST_ENABLE(0), \
-  .USER_ENABLE(0)) i_chan_cdc_``bus_name``_slave ( \
+  .USER_ENABLE(0), \
+  .RAM_PIPELINE(3), \
+  .OUTPUT_FIFO_ENABLE(1) \
+) i_chan_cdc_``bus_name``_slave ( \
   .s_clk(eci_clk), \
   .s_rst(eci_reset), \
   .s_axis_tdata({bus_name``_``data_name``_i, bus_name``_pkt_size_i, bus_name``_pkt_vc_i}), \
@@ -36,7 +39,10 @@ axis_async_fifo #( \
   .DATA_WIDTH((data_width) + 4 + ECI_PACKET_SIZE_WIDTH), \
   .KEEP_ENABLE(0), \
   .LAST_ENABLE(0), \
-  .USER_ENABLE(0)) i_chan_cdc_``bus_name``_master ( \
+  .USER_ENABLE(0), \
+  .RAM_PIPELINE(3), \
+  .OUTPUT_FIFO_ENABLE(1) \
+) i_chan_cdc_``bus_name``_master ( \
   .s_clk(app_clk), \
   .s_rst(app_reset), \
   .s_axis_tdata({cdc_``bus_name``_``data_name``_o, cdc_``bus_name``_pkt_size_o, cdc_``bus_name``_pkt_vc_o}), \
@@ -74,106 +80,106 @@ module dcs_cdc #(
    parameter PERF_REGS_WIDTH = 32,
    parameter SYNTH_PERF_REGS = 1 //0,1
 ) (
-    input logic 					   eci_clk,
-    input logic 					   eci_reset,
+    input logic                                   eci_clk,
+    input logic                                   eci_reset,
 
     // ECI channels
-    input logic [ECI_WORD_WIDTH-1:0] 			   req_wod_hdr_i,
-    input logic [ECI_PACKET_SIZE_WIDTH-1:0] 		   req_wod_pkt_size_i,
-    input logic [3:0] 					   req_wod_pkt_vc_i,
-    input logic 					   req_wod_pkt_valid_i,
-    output logic 					   req_wod_pkt_ready_o,
-    
-    input logic [ECI_WORD_WIDTH-1:0] 			   rsp_wod_hdr_i,
-    input logic [ECI_PACKET_SIZE_WIDTH-1:0] 		   rsp_wod_pkt_size_i,
-    input logic [3:0] 					   rsp_wod_pkt_vc_i,
-    input logic 					   rsp_wod_pkt_valid_i,
-    output logic 					   rsp_wod_pkt_ready_o,
+    input logic [ECI_WORD_WIDTH-1:0]              req_wod_hdr_i,
+    input logic [ECI_PACKET_SIZE_WIDTH-1:0]       req_wod_pkt_size_i,
+    input logic [3:0]                             req_wod_pkt_vc_i,
+    input logic                                   req_wod_pkt_valid_i,
+    output logic                                  req_wod_pkt_ready_o,
 
-    input logic [ECI_PACKET_SIZE-1:0][ECI_WORD_WIDTH-1:0]  rsp_wd_pkt_i,
-    input logic [ECI_PACKET_SIZE_WIDTH-1:0] 		   rsp_wd_pkt_size_i,
-    input logic [3:0] 					   rsp_wd_pkt_vc_i,
-    input logic 					   rsp_wd_pkt_valid_i,
-    output logic 					   rsp_wd_pkt_ready_o,
+    input logic [ECI_WORD_WIDTH-1:0]              rsp_wod_hdr_i,
+    input logic [ECI_PACKET_SIZE_WIDTH-1:0]       rsp_wod_pkt_size_i,
+    input logic [3:0]                             rsp_wod_pkt_vc_i,
+    input logic                                   rsp_wod_pkt_valid_i,
+    output logic                                  rsp_wod_pkt_ready_o,
 
-    output logic [ECI_WORD_WIDTH-1:0] 			   rsp_wod_hdr_o,
-    output logic [ECI_PACKET_SIZE_WIDTH-1:0] 		   rsp_wod_pkt_size_o,
-    output logic [3:0] 					   rsp_wod_pkt_vc_o,
-    output logic 					   rsp_wod_pkt_valid_o,
-    input logic 					   rsp_wod_pkt_ready_i,
+    input logic [ECI_PACKET_SIZE-1:0][ECI_WORD_WIDTH-1:0] rsp_wd_pkt_i,
+    input logic [ECI_PACKET_SIZE_WIDTH-1:0]       rsp_wd_pkt_size_i,
+    input logic [3:0]                             rsp_wd_pkt_vc_i,
+    input logic                                   rsp_wd_pkt_valid_i,
+    output logic                                  rsp_wd_pkt_ready_o,
+
+    output logic [ECI_WORD_WIDTH-1:0]             rsp_wod_hdr_o,
+    output logic [ECI_PACKET_SIZE_WIDTH-1:0]      rsp_wod_pkt_size_o,
+    output logic [3:0]                            rsp_wod_pkt_vc_o,
+    output logic                                  rsp_wod_pkt_valid_o,
+    input logic                                   rsp_wod_pkt_ready_i,
 
     output logic [ECI_PACKET_SIZE-1:0][ECI_WORD_WIDTH-1:0] rsp_wd_pkt_o,
-    output logic [ECI_PACKET_SIZE_WIDTH-1:0] 		   rsp_wd_pkt_size_o,
-    output logic [3:0] 					   rsp_wd_pkt_vc_o,
-    output logic 					   rsp_wd_pkt_valid_o,
-    input logic 					   rsp_wd_pkt_ready_i,
+    output logic [ECI_PACKET_SIZE_WIDTH-1:0]      rsp_wd_pkt_size_o,
+    output logic [3:0]                            rsp_wd_pkt_vc_o,
+    output logic                                  rsp_wd_pkt_valid_o,
+    input logic                                   rsp_wd_pkt_ready_i,
 
-    output logic [ECI_WORD_WIDTH-1:0] 			   fwd_wod_hdr_o,
-    output logic [ECI_PACKET_SIZE_WIDTH-1:0] 		   fwd_wod_pkt_size_o,
-    output logic [3:0] 					   fwd_wod_pkt_vc_o,
-    output logic 					   fwd_wod_pkt_valid_o,
-    input logic 					   fwd_wod_pkt_ready_i,
+    output logic [ECI_WORD_WIDTH-1:0]             fwd_wod_hdr_o,
+    output logic [ECI_PACKET_SIZE_WIDTH-1:0]      fwd_wod_pkt_size_o,
+    output logic [3:0]                            fwd_wod_pkt_vc_o,
+    output logic                                  fwd_wod_pkt_valid_o,
+    input logic                                   fwd_wod_pkt_ready_i,
 
     // app clocks
-    input logic 				           app_clk,
-    input logic					           app_reset,
+    input logic                                   app_clk,
+    input logic                                   app_reset,
 
     // LCL channels
-    input logic [ECI_WORD_WIDTH-1:0] 			   lcl_fwd_wod_hdr_i,
-    input logic [ECI_PACKET_SIZE_WIDTH-1:0] 		   lcl_fwd_wod_pkt_size_i,
-    input logic [ECI_LCL_TOT_NUM_VCS_WIDTH-1:0] 	   lcl_fwd_wod_pkt_vc_i, //5
-    input logic 					   lcl_fwd_wod_pkt_valid_i,
-    output logic 					   lcl_fwd_wod_pkt_ready_o,
+    input logic [ECI_WORD_WIDTH-1:0]              lcl_fwd_wod_hdr_i,
+    input logic [ECI_PACKET_SIZE_WIDTH-1:0]       lcl_fwd_wod_pkt_size_i,
+    input logic [ECI_LCL_TOT_NUM_VCS_WIDTH-1:0]   lcl_fwd_wod_pkt_vc_i, //5
+    input logic                                   lcl_fwd_wod_pkt_valid_i,
+    output logic                                  lcl_fwd_wod_pkt_ready_o,
 
-    input logic [ECI_WORD_WIDTH-1:0] 			   lcl_rsp_wod_hdr_i,
-    input logic [ECI_PACKET_SIZE_WIDTH-1:0] 		   lcl_rsp_wod_pkt_size_i,
-    input logic [ECI_LCL_TOT_NUM_VCS_WIDTH-1:0] 	   lcl_rsp_wod_pkt_vc_i,
-    input logic 					   lcl_rsp_wod_pkt_valid_i,
-    output logic 					   lcl_rsp_wod_pkt_ready_o,
+    input logic [ECI_WORD_WIDTH-1:0]              lcl_rsp_wod_hdr_i,
+    input logic [ECI_PACKET_SIZE_WIDTH-1:0]       lcl_rsp_wod_pkt_size_i,
+    input logic [ECI_LCL_TOT_NUM_VCS_WIDTH-1:0]   lcl_rsp_wod_pkt_vc_i,
+    input logic                                   lcl_rsp_wod_pkt_valid_i,
+    output logic                                  lcl_rsp_wod_pkt_ready_o,
 
-    output logic [ECI_WORD_WIDTH-1:0] 			   lcl_rsp_wod_hdr_o,
-    output logic [ECI_PACKET_SIZE_WIDTH-1:0] 		   lcl_rsp_wod_pkt_size_o,
-    output logic [ECI_LCL_TOT_NUM_VCS_WIDTH-1:0] 	   lcl_rsp_wod_pkt_vc_o,//5
-    output logic 					   lcl_rsp_wod_pkt_valid_o,
-    input logic 					   lcl_rsp_wod_pkt_ready_i,
+    output logic [ECI_WORD_WIDTH-1:0]             lcl_rsp_wod_hdr_o,
+    output logic [ECI_PACKET_SIZE_WIDTH-1:0]      lcl_rsp_wod_pkt_size_o,
+    output logic [ECI_LCL_TOT_NUM_VCS_WIDTH-1:0]  lcl_rsp_wod_pkt_vc_o,//5
+    output logic                                  lcl_rsp_wod_pkt_valid_o,
+    input logic                                   lcl_rsp_wod_pkt_ready_i,
 
     // AXI
-    output logic [AXI_ID_WIDTH-1:0] 			   p_axi_arid,
-    output logic [AXI_ADDR_WIDTH-1:0] 			   p_axi_araddr,
-    output logic [7:0] 					   p_axi_arlen, 
-    output logic [2:0] 					   p_axi_arsize, 
-    output logic [1:0] 					   p_axi_arburst, 
-    output logic 					   p_axi_arlock, 
-    output logic [3:0] 					   p_axi_arcache, 
-    output logic [2:0] 					   p_axi_arprot, 
-    output logic 					   p_axi_arvalid,
-    input logic 					   p_axi_arready,
-    input logic [AXI_ID_WIDTH-1:0] 			   p_axi_rid,
-    input logic [AXI_DATA_WIDTH-1:0] 			   p_axi_rdata,
-    input logic [1:0] 					   p_axi_rresp,
-    input logic 					   p_axi_rlast,
-    input logic 					   p_axi_rvalid,
-    output logic 					   p_axi_rready,
+    output logic [AXI_ID_WIDTH-1:0]     p_axi_arid,
+    output logic [AXI_ADDR_WIDTH-1:0]   p_axi_araddr,
+    output logic [7:0]                  p_axi_arlen,
+    output logic [2:0]                  p_axi_arsize,
+    output logic [1:0]                  p_axi_arburst,
+    output logic                        p_axi_arlock,
+    output logic [3:0]                  p_axi_arcache,
+    output logic [2:0]                  p_axi_arprot,
+    output logic                        p_axi_arvalid,
+    input logic                         p_axi_arready,
+    input logic [AXI_ID_WIDTH-1:0]      p_axi_rid,
+    input logic [AXI_DATA_WIDTH-1:0]    p_axi_rdata,
+    input logic [1:0]                   p_axi_rresp,
+    input logic                         p_axi_rlast,
+    input logic                         p_axi_rvalid,
+    output logic                        p_axi_rready,
 
-    output logic [AXI_ID_WIDTH-1:0] 			   p_axi_awid,
-    output logic [AXI_ADDR_WIDTH-1:0] 			   p_axi_awaddr,
-    output logic [7:0] 					   p_axi_awlen,
-    output logic [2:0] 					   p_axi_awsize,
-    output logic [1:0] 					   p_axi_awburst,
-    output logic 					   p_axi_awlock,
-    output logic [3:0] 					   p_axi_awcache,
-    output logic [2:0] 					   p_axi_awprot,
-    output logic 					   p_axi_awvalid,
-    input logic 					   p_axi_awready,
-    output logic [AXI_DATA_WIDTH-1:0] 			   p_axi_wdata,
-    output logic [AXI_STRB_WIDTH-1:0] 			   p_axi_wstrb,
-    output logic 					   p_axi_wlast,
-    output logic 					   p_axi_wvalid,
-    input logic 					   p_axi_wready,
-    input logic [AXI_ID_WIDTH-1:0] 			   p_axi_bid,
-    input logic [1:0] 					   p_axi_bresp,
-    input logic 					   p_axi_bvalid,
-    output logic 					   p_axi_bready
+    output logic [AXI_ID_WIDTH-1:0]     p_axi_awid,
+    output logic [AXI_ADDR_WIDTH-1:0]   p_axi_awaddr,
+    output logic [7:0]                  p_axi_awlen,
+    output logic [2:0]                  p_axi_awsize,
+    output logic [1:0]                  p_axi_awburst,
+    output logic                        p_axi_awlock,
+    output logic [3:0]                  p_axi_awcache,
+    output logic [2:0]                  p_axi_awprot,
+    output logic                        p_axi_awvalid,
+    input logic                         p_axi_awready,
+    output logic [AXI_DATA_WIDTH-1:0]   p_axi_wdata,
+    output logic [AXI_STRB_WIDTH-1:0]   p_axi_wstrb,
+    output logic                        p_axi_wlast,
+    output logic                        p_axi_wvalid,
+    input logic                         p_axi_wready,
+    input logic [AXI_ID_WIDTH-1:0]      p_axi_bid,
+    input logic [1:0]                   p_axi_bresp,
+    input logic                         p_axi_bvalid,
+    output logic                        p_axi_bready
 );
 
 `CROSS_CHAN_SLAVE(req_wod, hdr, ECI_WORD_WIDTH)
@@ -221,12 +227,12 @@ dcs_2_axi #(
 
   .p_axi_arid,
   .p_axi_araddr,
-  .p_axi_arlen, 
-  .p_axi_arsize, 
-  .p_axi_arburst, 
-  .p_axi_arlock, 
-  .p_axi_arcache, 
-  .p_axi_arprot, 
+  .p_axi_arlen,
+  .p_axi_arsize,
+  .p_axi_arburst,
+  .p_axi_arlock,
+  .p_axi_arcache,
+  .p_axi_arprot,
   .p_axi_arvalid,
   .p_axi_arready,
   .p_axi_rid,
