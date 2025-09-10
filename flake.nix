49f7ec02d697a61adc1b1415a0455dea6b22ba43
Pengcheng Xu <pengxu@ethz.ch>
2025-09-10 18:14:53 +0200

    configure linux-noble source for building module

--- flake.nix
+++ flake.nix
@@ -14,8 +14,8 @@
       name = "millw";
       nativeBuildInputs = [ pkgs.makeWrapper ];
       src = pkgs.fetchurl {
-          url = "https://raw.githubusercontent.com/com-lihaoyi/mill/f5d0c9f87ac58795323904c2cce44c105e652b50/mill";
-          hash = "sha256-yXCQ5YR0dOxjvC3beZDF0O3pVUXbZIyeKiEwU1nxWEw=";
+        url = https://raw.githubusercontent.com/com-lihaoyi/mill/f5d0c9f87ac58795323904c2cce44c105e652b50/mill;
+        hash = "sha256-yXCQ5YR0dOxjvC3beZDF0O3pVUXbZIyeKiEwU1nxWEw=";
       };
       phases = [ "installPhase" ];
       installPhase = ''
@@ -26,7 +26,29 @@
           --add-flags "--no-server"
       '';
     };
+    # aarch64 cross compiler
+    crossGcc = pkgs.pkgsCross.aarch64-multiplatform.buildPackages.gcc;
+    linux-noble-src = pkgs.stdenv.mkDerivation {
+      name = "linux-noble-src";
+      version = "6.8.0-64.67";
+      src = pkgs.fetchgit {
+        url = https://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/noble;
+        tag = "Ubuntu-6.8.0-64.67";
+        hash = "sha256-F2bcvzxlE2wzSj2kr+Fj9Ui6ht6GRv4p/hRSuSyglCc=";
+      };
+      nativeBuildInputs = with pkgs; [ flex bison bc openssl libelf crossGcc ];
+      buildPhase = ''
+        make ARCH=arm64 CROSS_COMPILE=aarch64-unknown-linux-gnu- defconfig
+        make ARCH=arm64 CROSS_COMPILE=aarch64-unknown-linux-gnu- modules_prepare
+      '';
+      installPhase = ''
+        mkdir -p $out
+        cp -a . $out/
+      '';
+      dontFixup = true;
+    };
   in {
+    packages = { inherit linux-noble-src; };
     devShells.default = with pkgs; let
       repeatTest = writeShellApplication {
         name = "repeat-test";
@@ -43,8 +65,7 @@
         zlib.dev verilator clang
         gtkwave sby yices
         jdk millw cmake
-        # cross compiler
-        pkgsCross.aarch64-multiplatform.buildPackages.gcc
+        crossGcc
         # quick script to repeat known failing test to find a good reproducer
         repeatTest
       ];
