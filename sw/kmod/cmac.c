6dccff4d108947010df21a3166cb0b41cec53dd9
Pengcheng Xu <pengxu@ethz.ch>
2025-10-17 18:06:12 +0200

    properly start cmac

--- sw/kmod/cmac.c
+++ sw/kmod/cmac.c
@@ -6,55 +6,56 @@
 #include "common.h"
 #include "cmac_dev.h"
 
-#define LINE_UP_MAX_ATTEMPTS 100
+// Total wait time 1000 ms
+#define LINE_UP_MAX_ATTEMPTS 1000
+#define LINE_UP_WAIT_MS 1
 
 int start_cmac(cmac_t *cmac, bool loopback)
 {
+	// CMAC lane alignment handshake:
+	// - enable RX and send RFI
+	// - wait RX_aligned
+	// - stop sending RFI
+	// - wait RX_status
+
 	cmac_stat_rx_status_t status;
-
-	cmac_conf_rx_1_rawwr(cmac, cmac_conf_rx_1_ctl_rx_enable_insert(
-					   0, 1)); // ctl_rx_enable
-	cmac_conf_tx_1_rawwr(cmac, cmac_conf_tx_1_ctl_tx_send_rfi_insert(
-					   0, 1)); // ctl_tx_send_rfi
-
 	int attempts = 0;
 
+	// ctl_rx_enable
+	cmac_conf_rx_1_ctl_rx_enable_wrf(cmac, 1);
+	// !ctl_tx_enable, ctl_tx_send_rfi
+	cmac_conf_tx_1_ctl_tx_send_rfi_wrf(cmac, 1);
+
 	while (++attempts < LINE_UP_MAX_ATTEMPTS) {
-		cmac_tick_rawwr(cmac, cmac_tick_tick_reg_insert(0, 1));
-		status = cmac_stat_rx_status_rd(cmac);
+		status = cmac_stat_rx_status_rawrd(cmac);
 		if (cmac_stat_rx_status_stat_rx_aligned_extract(status))
 			break; // RX_aligned
-		udelay(1000);
+		mdelay(LINE_UP_WAIT_MS);
 	}
 
 	if (attempts == LINE_UP_MAX_ATTEMPTS) {
 		pr_err("Wait for RX_aligned timed out\n");
-		return -1;
+		return -EBUSY;
 	}
+
+	// ctl_tx_enable, !ctl_tx_send_rfi
+	cmac_conf_tx_1_rawwr(cmac, cmac_conf_tx_1_ctl_tx_enable_insert(0, 1));
+
 	attempts = 0;
-
-	cmac_conf_tx_1_rawwr(cmac,
-			     cmac_conf_tx_1_ctl_tx_enable_insert(
-				     0, 1)); // ctl_tx_enable, !ctl_tx_send_rfi
-
-	// FIXME: why do we need this?
 	while (++attempts < LINE_UP_MAX_ATTEMPTS) {
-		cmac_tick_rawwr(cmac, cmac_tick_tick_reg_insert(0, 1));
-		status = cmac_stat_rx_status_rd(cmac);
+		status = cmac_stat_rx_status_rawrd(cmac);
 		if (cmac_stat_rx_status_stat_rx_aligned_extract(status) &&
 		    cmac_stat_rx_status_stat_rx_status_extract(status))
 			break; // RX_aligned && RX_status
-		udelay(1000);
+		mdelay(LINE_UP_WAIT_MS);
 	}
 
 	if (attempts == LINE_UP_MAX_ATTEMPTS) {
 		pr_err("Wait for RX_aligned && RX_status timed out\n");
-		return -1;
+		return -EBUSY;
 	}
 
-	cmac_gt_loopback_rawwr(
-		cmac, cmac_gt_loopback_ctl_gt_loopback_insert(0, loopback));
-
+	cmac_gt_loopback_ctl_gt_loopback_wrf(cmac, loopback);
 	pr_info("Loopback enabled: %s\n", loopback ? "true" : "false");
 
 	// flow control disabled - skipping regs
@@ -63,6 +64,6 @@ int start_cmac(cmac_t *cmac, bool loopback)
 
 void stop_cmac(cmac_t *cmac)
 {
-	cmac_conf_rx_fc_ctl_1_rawwr(cmac, 0); // !ctl_rx_enable
-	cmac_conf_tx_1_rawwr(cmac, 0); // !ctl_tx_enable
+	cmac_conf_rx_1_ctl_rx_enable_wrf(cmac, 0); // !ctl_rx_enable
+	cmac_conf_tx_1_ctl_tx_enable_wrf(cmac, 0); // !ctl_tx_enable
 }
