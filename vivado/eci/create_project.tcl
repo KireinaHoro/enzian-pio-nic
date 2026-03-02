af5d0717cdff1931570f7fef980607c34fbd0413
Pengcheng Xu <pengcheng.xu@inf.ethz.ch>
2026-03-02 21:59:54 +0100

    add missing file

--- vivado/eci/create_project.tcl
+++ vivado/eci/create_project.tcl
@@ -115,6 +115,7 @@ add_files -fileset [get_filesets sources_1] -norecurse \
 # Add sources from us
 add_files -fileset [get_filesets sources_1] -norecurse \
     "$src_dir/rtl/dcs_2_axi.sv" \
+    "$src_dir/rtl/dcs_trace_eci.sv" \
     "$src_dir/rtl/dcs_cdc.sv" \
     "$src_dir/rtl/${top_module}.vhd" \
     "$src_dir/rtl/axil_regs_interconnect.v" \
